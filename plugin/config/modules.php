4fb5e2c4f80becbb510da65b47dd1328733ee8da
Przemysław Żydek <przemyslaw.z@mpcreation.net>
2019-07-18 10:30:56 +0200

    Imported namespace

--- plugin/config/modules.php
+++ plugin/config/modules.php
@@ -6,6 +6,8 @@
  * In order to add module you only need to add reference to it's class (that extends base Module class) to array below.
  */
 
+use UnderScorer\Core\CoreModule;
+
 return apply_filters( 'wpk/modules', [
-    'core' => \UnderScorer\Core\CoreModule::class,
+    'core' => CoreModule::class,
 ] );
