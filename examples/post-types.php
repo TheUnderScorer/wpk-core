21aabaae23105d8fae07fcc72763ea669a6ef718
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-08-27 20:26:51 +0200

    Updated examples

--- examples/post-types.php
+++ examples/post-types.php
@@ -1,52 +1,12 @@
 <?php
 
-use UnderScorer\Core\Admin\Menu;
-use UnderScorer\Core\Creators\PostTypeCreator;
+$postType = tr_post_type( 'Note' );
 
-$creator = new PostTypeCreator( 'my_post_type', [
-    /**
-     * @see register_post_type() for args
-     */
-] );
+$detailsMetabox = tr_meta_box( 'Note Details' );
+$postType->addMetaBox( $detailsMetabox );
 
-$creator
-    ->addTaxonomy( 'some_taxonomy', [
-        /**
-         * @see register_taxonomy() for args
-         */
-    ] );
+$detailsMetabox->setCallback( function () {
+    $form = tr_form( 'post' );
 
-// Regular column
-$creator
-    ->addAdminColumn(
-        'my_column',
-        'My column',
-        function ( int $postID ) {
-            $meta = get_post_meta( $postID, 'some_meta', true );
-
-            echo $meta;
-        }
-    );
-
-// Sortable column
-$creator
-    ->addSortableColumn(
-        'my_sortable_column',
-        'Sortable column',
-        function ( int $postID ) {
-            $meta = get_post_meta( $postID, 'some_meta', true );
-
-            echo $meta;
-        },
-        );
-
-// Submenu that get's attached to post type menu in dashboard
-$creator->addSubmenu(
-    new Menu( 'my_post_menu', [
-        'menuTitle' => 'Some title',
-        'pageTitle' => 'Some title',
-        'callback'  => function () {
-            echo 'Hello world!';
-        },
-    ] )
-);
+    $form->text( 'Note Details' );
+} );
